6d0da73821f383814b12eeebaf200b5d737403ec
Sunil Mane <Sunil.Mane@harman.com>
2018-10-09 18:28:48 +0530

    Added style to gif search component

--- src/app/gif-search/gif-search.component.ts
+++ src/app/gif-search/gif-search.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { GifSearchService } from '../services/gif-search.service';
 
 @Component({
   selector: 'app-gif-search',
   templateUrl: './gif-search.component.html',
-  styleUrls: ['./gif-search.component.css']
+  styleUrls: ['./gif-search.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class GifSearchComponent implements OnInit {
   gifSearchForm: FormGroup;
